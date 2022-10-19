85c393845da28e82164c714b82be33dd026b8a35
A01254100 <112582780+A01254100@users.noreply.github.com>
2022-10-19 12:51:53 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-input.onPinPressed(TouchPin.P0, function () {
-    control.reset()
-})
+let Man: game.LedSprite = null
+let Pedrito: game.LedSprite = null
+let Andrea: game.LedSprite = null
 let Javier = game.createSprite(0, 2)
-let Andrea = game.createSprite(randint(1, 4), randint(0, 4))
-let Pedrito = game.createSprite(randint(1, 4), randint(0, 4))
-let Man = game.createSprite(randint(1, 4), randint(0, 4))
 Andrea.set(LedSpriteProperty.Brightness, 100)
 Pedrito.set(LedSpriteProperty.Brightness, 110)
 Man.set(LedSpriteProperty.Brightness, 120)
+Andrea = game.createSprite(randint(1, 4), randint(0, 4))
+Pedrito = game.createSprite(randint(1, 4), randint(0, 4))
+Man = game.createSprite(randint(1, 4), randint(0, 4))
 basic.forever(function () {
     while (input.buttonIsPressed(Button.A)) {
         Javier.change(LedSpriteProperty.X, -1)
@@ -26,8 +26,19 @@ basic.forever(function () {
         basic.pause(100)
     }
     if (Javier.isTouching(Andrea)) {
-        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.OnceInBackground)
+        game.addScore(1)
         Andrea.delete()
+    }
+    if (Javier.isTouching(Man)) {
+        game.addScore(1)
+        Man.delete()
+    }
+    if (Javier.isTouching(Pedrito)) {
+        game.addScore(1)
+        Pedrito.delete()
+    }
+    if (game.score() == 3) {
+        music.startMelody(music.builtInMelody(Melodies.Funk), MelodyOptions.OnceInBackground)
         for (let index = 0; index < 3; index++) {
             basic.showIcon(IconNames.Yes)
             basic.clearScreen()
@@ -37,5 +48,10 @@ basic.forever(function () {
     }
 })
 loops.everyInterval(3000, function () {
-	
+    Andrea = game.createSprite(randint(1, 4), randint(0, 4))
+    Pedrito = game.createSprite(randint(1, 4), randint(0, 4))
+    Man = game.createSprite(randint(1, 4), randint(0, 4))
+    Andrea.delete()
+    Man.delete()
+    Pedrito.delete()
 })
